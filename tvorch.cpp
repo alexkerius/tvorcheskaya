5fbccc18324546b5c43d0a8fb7fa751dc776530e
unter <86520137+alexkerius@users.noreply.github.com>
2022-12-21 00:57:15 +0300

    Update tvorch.cpp

--- tvorch.cpp
+++ tvorch.cpp
@@ -41,19 +41,21 @@ class Student : public Human
 public:
     vector<int> Marks;
     Student(string name,int age, int weight, int height) : Human(name,  age,  weight,  height){}
+    Student() : Human(){}
     double DefinePrimaryRating(){
-        int sum = 0;
+        double sum = 0;
         if(Name == DefaultName){
             return 0;
         }
+
         sort(Marks.begin(),Marks.end());
+        Marks.pop_back();
         Marks.erase(Marks.begin());
-        Marks.erase(Marks.end());
+        
         for(int i = 0; i < Marks.size(); i++){
             sum += Marks[i];
-            cout << sum << '\n';
         }
-        return sum/Marks.size();
+        return sum/(Marks.size());
     }
 
     double DefineSecondaryRating(){
@@ -64,10 +66,28 @@ public:
 };
 
 
-
-
 int main(){
-    Student alex("Alex",16,60,170);
-    alex.Marks = {1,2,3,4,5};
-    alex.DefinePrimaryRating();
+    Student students[5] = {Student("Alex",16,60,170),
+        Student("Egor",17,86,190),
+        Student("Ann",16,40,150),
+        Student("Zlata",16,50,178),
+        Student("Charles",15,80,175)};
+    for(int i = 0; i < 5; i++){
+        vector<int> marks_for_one;
+        for(int j = 0; j < 5; j++){
+            int g;
+            cin >> g;
+            marks_for_one.push_back(g);
+        }
+        students[i].Marks = marks_for_one;
+    }
+
+    Student best("None",0,0,0);
+    best.Marks = {0,0,0,0,0};
+    for(int i = 0; i < 5; i++){
+        
+        
+    }
+    cout << "The best student is "<< best.Name;
+    return 0;
 }
